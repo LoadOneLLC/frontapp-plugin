a1d29d2951a5399e2ae845cd6802b8d136a55143
Paito Anderson <pj.paito@gmail.com>
2023-02-20 11:11:27 -0500

    Improve styles

--- src/components/AgentNotes.tsx
+++ src/components/AgentNotes.tsx
@@ -39,16 +39,12 @@ function AgentNotes() {
       <h4 className='text-xl dark:text-white'>AGENT NOTES</h4>
       {topCustomer && <p className='dark:text-red-600'>Top Customer</p>}
       <p className='dark:text-slate-200'>{agentNote}</p>
-      <br />
       <div>
         {billingCustomerId && <>
-          <button onClick={() => Front.openUrl(`https://app.load1.com/Customers/Customer/${billingCustomerId}`)}>Billing Customer</button>
-          <br />
+          <button className="px-4 py-2 mb-1 d-block w-full font-semibold text-sm bg-sky-600 text-white rounded-full shadow-sm" onClick={() => Front.openUrl(`https://app.load1.com/Customers/Customer/${billingCustomerId}`)}>Billing Customer</button>
         </>}
-        <button onClick={() => Front.openUrl(`https://app.load1.com/Quote/Index?frontId=${context.conversation.id}`)}>Quote</button>
-        <br />
-        <button onClick={() => Front.openUrl(`https://app.load1.com/Quote/BlindBidQuote?frontId=${context.conversation.id}`)}>Blind Bid Quote</button>
-        <br />
+        <button className="px-4 py-2 mb-1 d-block w-full font-semibold text-sm bg-sky-600 text-white rounded-full shadow-sm" onClick={() => Front.openUrl(`https://app.load1.com/Quote/Index?frontId=${context.conversation.id}`)}>Quote</button>
+        <button className="px-4 py-2 mb-1 d-block w-full font-semibold text-sm bg-sky-600 text-white rounded-full shadow-sm" onClick={() => Front.openUrl(`https://app.load1.com/Quote/BlindBidQuote?frontId=${context.conversation.id}`)}>Blind Bid Quote</button>
       </div>
     </>
   );
--- src/components/CarrierGroups.tsx
+++ src/components/CarrierGroups.tsx
@@ -1,10 +1,16 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, Fragment } from 'react';
+import { Combobox, Menu, Transition } from '@headlessui/react';
+import { CheckIcon, ChevronDownIcon, ChevronUpDownIcon } from '@heroicons/react/20/solid';
 
 type CarrierGroup = {
   GroupID: number;
   Name: string;
-  VehicleTypeName: string;
+  VehicleTypeName: string | null;
 };
+
+function classNames(...classes: string[]) {
+  return classes.filter(Boolean).join(' ')
+}
 
 function CarrierGroups() {
   const [loading, setLoading] = useState(true);
@@ -25,18 +31,60 @@ function CarrierGroups() {
       });
   }, []);
 
+  const filteredCarriers =
+    filter === ''
+      ? carrierGroups
+      : carrierGroups.filter((carrierGroup) => {
+          return carrierGroup.Name.toLowerCase().includes(filter.toLowerCase())
+        })
+
   return (
     <>
-      <select>
-        {carrierGroups.map((carrierGroup) => (
-          <option
-            key={carrierGroup.GroupID}
-            onClick={() => console.log(carrierGroup)}
+      <Combobox onChange={(value) => console.log(value)}>
+        <div className="relative mt-1">
+          <div className="relative w-full cursor-default overflow-hidden rounded-lg bg-white text-left shadow-md focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-teal-300 sm:text-sm">
+            <Combobox.Input
+              className="w-full border-none py-2 pl-3 pr-10 text-sm leading-5 text-gray-900 focus:ring-0"
+              placeholder="Copy Carrier Emails"
+              onChange={(event) => setFilter(event.target.value)}
+            />
+            <Combobox.Button className="absolute inset-y-0 right-0 flex items-center pr-2">
+              <ChevronUpDownIcon
+                className="h-5 w-5 text-gray-400"
+                aria-hidden="true"
+              />
+            </Combobox.Button>
+          </div>
+          <Transition
+            as={Fragment}
+            leave="transition ease-in duration-100"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+            afterLeave={() => setFilter('')}
           >
-            {carrierGroup.Name}
-          </option>
-        ))}
-      </select>
+            <Combobox.Options className="w-full rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5">
+              {filteredCarriers.length === 0 && filter !== '' ? (
+                <div className="relative cursor-default select-none py-2 px-4 text-gray-700">
+                  Nothing found.
+                </div>
+              ) : (
+              filteredCarriers.map((carrierGroup) => (
+                <Combobox.Option
+                  key={carrierGroup.GroupID}
+                  className={({ active }) => `relative cursor-default select-none py-2 pl-10 pr-4 text-gray-900`}
+                  value={carrierGroup}
+                >
+                  <span
+                    className="block truncate font-normal"
+                  >
+                    {carrierGroup.Name}
+                  </span>
+                </Combobox.Option>
+              )))}
+            </Combobox.Options>
+          </Transition>
+        </div>
+      </Combobox>
     </>
   );
 }
