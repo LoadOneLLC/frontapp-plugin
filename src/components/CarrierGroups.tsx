cd07f7311802772dddab659d79e651735b3f25b7
Paito Anderson <pj.paito@gmail.com>
2023-02-20 17:39:26 -0500

    Fix build

--- src/components/CarrierGroups.tsx
+++ src/components/CarrierGroups.tsx
@@ -12,6 +12,7 @@ type CarrierGroup = {
 function CarrierGroups() {
   const [loading, setLoading] = useState(true);
   const [carrierGroups, setCarrierGroups] = useState<CarrierGroup[]>([]);
+  const [filter, setFilter] = useState('');
 
   useEffect(() => {
     fetch('/Front/GetCarrierGroups', {
@@ -59,7 +60,7 @@ function CarrierGroups() {
           <div className="relative w-full cursor-default overflow-hidden rounded-lg bg-white text-left shadow-md focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-teal-300 sm:text-sm">
             <Combobox.Input
               className="w-full border-none py-2 pl-3 pr-10 text-sm leading-5 text-gray-900 focus:ring-0"
-              placeholder="Copy Carrier Emails"
+              placeholder="Filter Carrier Groups.."
               onChange={(event) => setFilter(event.target.value)}
             />
             <Combobox.Button className="absolute inset-y-0 right-0 flex items-center pr-2">
