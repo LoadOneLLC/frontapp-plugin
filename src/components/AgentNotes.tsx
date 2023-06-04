86794e424a55ce41e346cb294a455a5fc14b3081
Paito Anderson <pj.paito@gmail.com>
2023-06-04 11:32:42 -0400

    Add Create Quote AI

--- src/components/AgentNotes.tsx
+++ src/components/AgentNotes.tsx
@@ -1,12 +1,15 @@
 import { useFrontContext } from '../providers/frontContext';
 import Front, { SingleConversationContext } from '@frontapp/plugin-sdk';
 import { useEffect, useState } from 'react';
+import { toast } from 'react-toastify';
+import { JsonResponse } from '../TypeGen/json-response';
 
 function AgentNotes() {
   const context = useFrontContext() as SingleConversationContext;
   const [agentNote, setAgentNote] = useState("");
   const [billingCustomerId, setBillingCustomerId] = useState(0);
   const [topCustomer, setTopCustomer] = useState(false);
+  const [creatingQuote, setCreatingQuote] = useState(false);
 
   useEffect(() => {
     if (import.meta.env.DEV)
@@ -41,14 +44,49 @@ function AgentNotes() {
       });
   }, [context?.conversation.recipient]);
 
+  const createQuote = () => {
+    setCreatingQuote(true);
+    fetch('/Front/CreateQuote', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'bearer ' + new URLSearchParams(window.location.search).get('auth_secret'),
+      },
+      body: JSON.stringify({ 
+        ConversationID: context.conversation.id,
+        TeammateID: context.teammate.id,
+      })
+    })
+    .then(async (response) => {
+      var json = await response.json() as JsonResponse;
+      var quoteLink = json.Links.find(l => l.Name === 'self');
+      if (quoteLink) Front.openUrl(quoteLink.Link);
+    })
+    .catch(() => {
+      toast("Error creating quote!");
+    })
+    .finally(() => {
+      setCreatingQuote(false);
+    });
+  }
+
   return (
     <>
       <h4 className='text-xl dark:text-white'>AGENT NOTES</h4>
       {topCustomer && <p className='dark:text-red-600'>Top Customer</p>}
       <p className='dark:text-slate-200 mb-2'>{agentNote}</p>
-      {billingCustomerId > 0 ? <button className="px-4 py-2 mb-2 d-block w-full font-semibold text-sm bg-sky-600 hover:bg-sky-700 text-white rounded-md shadow-sm" onClick={() => Front.openUrl(`https://app.load1.com/Customers/Customer/${billingCustomerId}`)}>Billing Customer</button> : null}
-      <button className="px-4 py-2 mb-2 d-block w-full font-semibold text-sm bg-sky-600 hover:bg-sky-700 text-white rounded-md shadow-sm" onClick={() => Front.openUrl(`https://app.load1.com/Quote/Index?frontId=${context.conversation.id}`)}>Quote</button>
-      <button className="px-4 py-2 mb-2 d-block w-full font-semibold text-sm bg-sky-600 hover:bg-sky-700 text-white rounded-md shadow-sm" onClick={() => Front.openUrl(`https://app.load1.com/Quote/BlindBidQuote?frontId=${context.conversation.id}`)}>Blind Bid Quote</button>
+      {billingCustomerId > 0 ? <button className="px-4 py-2 mb-2 d-block w-full font-semibold text-sm bg-sky-600 hover:bg-sky-700 text-white rounded-md shadow-sm" onClick={() => Front.openUrl(`https://app.load1.com/Customers/Customer/${billingCustomerId}`)}>
+        Billing Customer
+      </button> : null}
+      <button className="px-4 py-2 mb-2 d-block w-full font-semibold text-sm bg-sky-600 hover:bg-sky-700 text-white rounded-md shadow-sm" onClick={() => Front.openUrl(`https://app.load1.com/Quote/Index?frontId=${context.conversation.id}`)}>
+        Build Quote
+      </button>
+      <button disabled={creatingQuote} className="px-4 py-2 mb-2 d-block w-full font-semibold text-sm bg-sky-600 hover:bg-sky-700 text-white rounded-md shadow-sm" onClick={createQuote}>
+        Create Quote <i>AI</i>
+      </button>
+      <button className="px-4 py-2 mb-2 d-block w-full font-semibold text-sm bg-sky-600 hover:bg-sky-700 text-white rounded-md shadow-sm" onClick={() => Front.openUrl(`https://app.load1.com/Quote/BlindBidQuote?frontId=${context.conversation.id}`)}>
+        Blind Bid Quote
+      </button>
     </>
   );
 }
